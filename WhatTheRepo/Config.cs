3cf07499f821f8d7223dcc38ed8142f59f06c623
ritomsonowal <ritomsonowal99@gmail.com>
2020-04-10 19:07:01 +0530

    Testing gitignore

--- WhatTheRepo/Config.cs
+++ WhatTheRepo/Config.cs
@@ -6,7 +6,7 @@ namespace WhatTheRepo
 {
     class Config
     {
-        public string CONSUMER_KEY = "Bot";
+        public string CONSUMER_KEY = "";
         public string CONSUMER_SECRET = "";
         public string ACCESS_TOKEN = "";
         public string ACCESS_TOKEN_SECRET = "";
