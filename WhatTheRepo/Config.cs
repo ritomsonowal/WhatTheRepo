33c7df541a3e94610655baefe6590da75004290a
ritomsonowal <ritomsonowal99@gmail.com>
2020-04-10 17:12:59 +0530

    Add project files.

---
+++ WhatTheRepo/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatTheRepo
+{
+    class Config
+    {
+        public string CONSUMER_KEY = "Bot";
+        public string CONSUMER_SECRET = "";
+        public string ACCESS_TOKEN = "";
+        public string ACCESS_TOKEN_SECRET = "";
+    }
+}
---
+++ WhatTheRepo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WhatTheRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config config = new Config();
+            Console.WriteLine("Hello " + config.CONSUMER_KEY);
+        }
+    }
+}
