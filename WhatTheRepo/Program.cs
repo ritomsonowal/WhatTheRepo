038527562a6ff34092e58a1dd3f2166c4cc5b1e0
ritomsonowal <ritomsonowal99@gmail.com>
2020-04-10 19:01:22 +0530

    Revert "Add twitter API"
    This reverts commit 55afe06ed935443b807594f04037db9cd7d894ac.

--- WhatTheRepo/Program.cs
+++ WhatTheRepo/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using Tweetinvi;
-using Tweetinvi.Models;
-using Tweetinvi.Exceptions;
 
 namespace WhatTheRepo
 {
@@ -10,27 +7,7 @@ namespace WhatTheRepo
         static void Main(string[] args)
         {
             Config config = new Config();
-
-            DateTime now = DateTime.Now;
-
-            // Set up your credentials 
-            Auth.SetUserCredentials(config.CONSUMER_KEY, config.CONSUMER_SECRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET);
-
-            ExceptionHandler.SwallowWebExceptions = false;
-
-            // Publish the Tweet "Hello World" on your Timeline
-            try
-            {
-                var status = Tweet.PublishTweet("Hello World! I'm a bot tweeting to twitter adsnnj.");
-                Console.WriteLine(" ");
-                Console.WriteLine("{0} ......... @whattherepo tweeted : {1} ", now.ToString("F"), status);
-                Console.WriteLine(" ");
-            }
-            catch (TwitterException ex)
-            {
-                // Twitter API Request has been failed; Bad request, network failure or unauthorized request
-                Console.WriteLine("Something went wrong when we tried to execute the http request : '{0}'", ex.TwitterDescription);
-            }
+            Console.WriteLine("Hello " + config.CONSUMER_KEY);
         }
     }
 }
