55afe06ed935443b807594f04037db9cd7d894ac
ritomsonowal <ritomsonowal99@gmail.com>
2020-04-10 18:56:49 +0530

    Add twitter API

--- WhatTheRepo/Config.cs
+++ WhatTheRepo/Config.cs
@@ -6,9 +6,9 @@ namespace WhatTheRepo
 {
     class Config
     {
-        public string CONSUMER_KEY = "Bot";
-        public string CONSUMER_SECRET = "";
-        public string ACCESS_TOKEN = "";
-        public string ACCESS_TOKEN_SECRET = "";
+        public string CONSUMER_KEY = "Gqr5EkmbxKZmglrI7Ih2VRGMY";
+        public string CONSUMER_SECRET = "w24Rjoxjk698x27oOfMnxlLoyQ38AvsWACrs9bZbodRlIEq4qE";
+        public string ACCESS_TOKEN = "1248481979772719104-z0dqcIcjDLDRxjSDrXFFKmkSAjaogn";
+        public string ACCESS_TOKEN_SECRET = "CcfrOKoZjseqGpdYatCjqYJdJYgRv28br93WNU3Bvl0BM";
     }
 }
--- WhatTheRepo/Program.cs
+++ WhatTheRepo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Exceptions;
 
 namespace WhatTheRepo
 {
@@ -7,7 +10,27 @@ namespace WhatTheRepo
         static void Main(string[] args)
         {
             Config config = new Config();
-            Console.WriteLine("Hello " + config.CONSUMER_KEY);
+
+            DateTime now = DateTime.Now;
+
+            // Set up your credentials 
+            Auth.SetUserCredentials(config.CONSUMER_KEY, config.CONSUMER_SECRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET);
+
+            ExceptionHandler.SwallowWebExceptions = false;
+
+            // Publish the Tweet "Hello World" on your Timeline
+            try
+            {
+                var status = Tweet.PublishTweet("Hello World! I'm a bot tweeting to twitter adsnnj.");
+                Console.WriteLine(" ");
+                Console.WriteLine("{0} ......... @whattherepo tweeted : {1} ", now.ToString("F"), status);
+                Console.WriteLine(" ");
+            }
+            catch (TwitterException ex)
+            {
+                // Twitter API Request has been failed; Bad request, network failure or unauthorized request
+                Console.WriteLine("Something went wrong when we tried to execute the http request : '{0}'", ex.TwitterDescription);
+            }
         }
     }
 }
